84796dba85bb0e89b000dde514561e03b0938758
trybefore <trybefore@pm.me>
2019-03-29 11:24:46 +0100

    initial commit, untested

---
+++ client/client.go
@@ -0,0 +1,85 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+type Endpoint string
+
+const (
+	SearchEndpoint           Endpoint = "search"
+	CategoriesEndpoint       Endpoint = "categories"
+	SearchSuggestionEndpoint Endpoint = "search_suggestions"
+	AutoCompleteEndpoint     Endpoint = "autocomplete"
+	TrendingTermsEndpoint    Endpoint = "trending_terms"
+	RegisterShareEndpoint    Endpoint = "registershare"
+	GIFSEndpoint             Endpoint = "gifs"
+	RandomEndpoint           Endpoint = "random"
+
+	tenorURL = "https://api.tenor.com/v1/%s?key=%s&"
+)
+
+// TenorClient client holding
+type TenorClient struct {
+	apiKey string
+	client *http.Client
+}
+
+// NewTenorClient returns a new tenorclient with the provided api key.
+func NewTenorClient(apiKey string) *TenorClient {
+	return &TenorClient{
+		apiKey: apiKey,
+		client: &http.Client{
+			Timeout: time.Second * 15,
+		},
+	}
+}
+
+// Search searches the endpoint using the provided query.
+// you do not need provide a key.
+func (t *TenorClient) Search(endpoint Endpoint, query map[string]interface{}) *TenorResponse {
+	return t.search(endpoint, query)
+}
+
+func (t *TenorClient) search(endpoint Endpoint, query map[string]interface{}) *TenorResponse {
+	response := TenorResponse{}
+
+	url := t.formURL(endpoint, query)
+	resp, err := t.client.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	err = json.Unmarshal(body, &response)
+
+	return &response
+}
+
+func (t *TenorClient) formURL(endpoint Endpoint, query map[string]interface{}) string {
+	u := fmt.Sprintf(tenorURL, string(endpoint), t.apiKey)
+
+	queries := make([]string, len(query))
+
+	for k, v := range query {
+		queries = append(queries, fmt.Sprintf("%s=%v", k, v))
+	}
+
+	q := formatQuery(strings.Join(queries, "&"))
+
+	u = u + q
+
+	return u
+}
+
+func formatQuery(u string) string {
+	return url.QueryEscape(u)
+}
---
+++ client/responses.go
@@ -0,0 +1,89 @@
+package client
+
+// TenorResponse the response from a tenor query.
+type TenorResponse struct {
+	Results []struct {
+		Tags  []interface{} `json:"tags"`
+		URL   string        `json:"url"`
+		Media []struct {
+			Nanomp4 struct {
+				URL      string  `json:"url"`
+				Dims     []int   `json:"dims"`
+				Duration float64 `json:"duration"`
+				Preview  string  `json:"preview"`
+				Size     int     `json:"size"`
+			} `json:"nanomp4"`
+			Nanowebm struct {
+				URL     string `json:"url"`
+				Dims    []int  `json:"dims"`
+				Preview string `json:"preview"`
+				Size    int    `json:"size"`
+			} `json:"nanowebm"`
+			Tinygif struct {
+				URL     string `json:"url"`
+				Dims    []int  `json:"dims"`
+				Preview string `json:"preview"`
+				Size    int    `json:"size"`
+			} `json:"tinygif"`
+			Tinymp4 struct {
+				URL      string  `json:"url"`
+				Dims     []int   `json:"dims"`
+				Duration float64 `json:"duration"`
+				Preview  string  `json:"preview"`
+				Size     int     `json:"size"`
+			} `json:"tinymp4"`
+			Tinywebm struct {
+				URL     string `json:"url"`
+				Dims    []int  `json:"dims"`
+				Preview string `json:"preview"`
+				Size    int    `json:"size"`
+			} `json:"tinywebm"`
+			Webm struct {
+				URL     string `json:"url"`
+				Dims    []int  `json:"dims"`
+				Preview string `json:"preview"`
+				Size    int    `json:"size"`
+			} `json:"webm"`
+			Gif struct {
+				URL     string `json:"url"`
+				Dims    []int  `json:"dims"`
+				Preview string `json:"preview"`
+				Size    int    `json:"size"`
+			} `json:"gif"`
+			Mp4 struct {
+				URL      string  `json:"url"`
+				Dims     []int   `json:"dims"`
+				Duration float64 `json:"duration"`
+				Preview  string  `json:"preview"`
+				Size     int     `json:"size"`
+			} `json:"mp4"`
+			Loopedmp4 struct {
+				URL      string  `json:"url"`
+				Dims     []int   `json:"dims"`
+				Duration float64 `json:"duration"`
+				Preview  string  `json:"preview"`
+				Size     int     `json:"size"`
+			} `json:"loopedmp4"`
+			Mediumgif struct {
+				URL     string `json:"url"`
+				Dims    []int  `json:"dims"`
+				Preview string `json:"preview"`
+				Size    int    `json:"size"`
+			} `json:"mediumgif"`
+			Nanogif struct {
+				URL     string `json:"url"`
+				Dims    []int  `json:"dims"`
+				Preview string `json:"preview"`
+				Size    int    `json:"size"`
+			} `json:"nanogif"`
+		} `json:"media"`
+		Created   float64     `json:"created"`
+		Shares    int         `json:"shares"`
+		Itemurl   string      `json:"itemurl"`
+		Composite interface{} `json:"composite"`
+		Hasaudio  bool        `json:"hasaudio"`
+		Title     string      `json:"title"`
+		ID        string      `json:"id"`
+	} `json:"results"`
+	Next string `json:"next"`
+}
